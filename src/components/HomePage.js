ea54e43edb173f11fa31168642e73d4f36bb6efd
Kalib Hackin <nagui08@hotmail.com>
2020-04-29 12:36:32 -0500

    adding new dependencies

---
+++ src/components/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const HomePage = () => (
+  <>
+    <aside className="side-bar">
+      <p>This is a side bar</p>
+    </aside>
+    <div className="home-page">
+      <h1>Here goes the main content!</h1>
+    </div>
+  </>
+);
+
+export default HomePage;
