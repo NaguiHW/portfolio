eaf5635275ffb7fc8a803d3a13c74c7466cb486f
Kalib Hackin <nagui08@hotmail.com>
2020-04-30 14:52:15 -0500

    homepage done

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -4,12 +4,16 @@ import '../assets/styles/HomePage.scss';
 
 const HomePage = () => (
   <div className="home-page">
-    <img src={image} alt="Kalib Hackin" />
+    <div className="image-container">
+      <img src={image} alt="Kalib Hackin" />
+    </div>
     <div className="bio">
       <h1>
         Kalib
+        {' '}
         <span>Hackin</span>
       </h1>
+      <div className="line" />
       <h3>Fullstack Developer</h3>
       <p>
         I am Kalb Hackin and I am a fullstack developer that use HTML,
