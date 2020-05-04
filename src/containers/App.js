fb813bc9a14b1370ba1339cb93fec51bda42a9ad
Kalib Hackin <nagui08@hotmail.com>
2020-05-04 11:41:58 -0500

    adding transition to sidebar

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import HomePage from '../components/HomePage';
+import SideBar from '../components/SideBar';
 
-const App = () => (
-  <>
-    <HomePage />
-  </>
-);
+const App = () => {
+  useEffect(() => {
+    document.addEventListener('scroll', () => {
+      const height = window.pageYOffset;
+      const sidebar = document.getElementById('sidebar');
+      sidebar.className = height >= 250 ? 'sidebar-scroll' : 'sidebar';
+      console.log(height);
+      console.log(sidebar);
+    });
+  });
+
+  return (
+    <>
+      <SideBar />
+      <HomePage />
+      <HomePage />
+      <HomePage />
+    </>
+  );
+};
 
 export default App;
