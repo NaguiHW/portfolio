f7e43acbc652e3d0c1137e5fce2bb0ecc3b49a19
Kalib Hackin <nagui08@hotmail.com>
2020-05-04 13:34:36 -0500

    styling project area

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,11 +3,12 @@ import lexusAppointment from '../assets/images/lexus-appointment.png';
 import facebookClone from '../assets/images/facebook-clone.png';
 import bookstore from '../assets/images/bookstore.png';
 import todoApp from '../assets/images/todo-app.png';
+import '../assets/styles/Projects.scss';
 
 const Projects = () => (
   <div className="projects-container">
     <h1>My Projects</h1>
-    <div>
+    <div className="projects-area">
       <div className="project">
         <img src={lexusAppointment} alt="Lexus Appointment" />
         <h3>Lexus Appointment</h3>
@@ -28,6 +29,30 @@ const Projects = () => (
           <h5>HTML</h5>
           <h5>CSS</h5>
           <h5>Ruby on Rails</h5>
+        </div>
+        <div className="links">
+
+        </div>
+      </div>
+      <div className="project">
+        <img src={todoApp} alt="To Do App" />
+        <h3>Lexus Appointment</h3>
+        <div className="technologies">
+          <h5>HTML</h5>
+          <h5>CSS</h5>
+          <h5>Javascript</h5>
+        </div>
+        <div className="links">
+
+        </div>
+      </div>
+      <div className="project">
+        <img src={bookstore} alt="Bookstore" />
+        <h3>Lexus Appointment</h3>
+        <div className="technologies">
+          <h5>HTML</h5>
+          <h5>CSS</h5>
+          <h5>React</h5>
         </div>
         <div className="links">
 
